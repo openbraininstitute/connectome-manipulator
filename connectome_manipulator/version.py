c67ff0c52dd12dd539fc378653507ef818b52dc9
Christoph Pokorny <christoph.pokorny@openbraininstitute.org>
2025-01-28 10:03:54 +0100

    Version bump

--- connectome_manipulator/version.py
+++ connectome_manipulator/version.py
@@ -1,9 +1,6 @@
 # This file is part of connectome-manipulator.
-#
-# SPDX-License-Identifier: Apache-2.0
-# Copyright (c) 2024 Blue Brain Project/EPFL
 
 """Package version."""
 
-VERSION = "1.0.2"
+VERSION = "1.0.3"
 __version__ = VERSION
