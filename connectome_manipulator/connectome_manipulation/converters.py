18965d24d88da896b6bd6fa9de0e3f22937d9883
Christoph Pokorny <christoph.pokorny@openbraininstitute.org>
2025-10-09 17:54:04 +0200

    Removed legacy keyword no longer used in ParquetDataset

--- connectome_manipulator/connectome_manipulation/converters.py
+++ connectome_manipulator/connectome_manipulation/converters.py
@@ -205,7 +205,7 @@ def create_parquet_metadata(parquet_path, nodes):
         log.warning("No .parquet files exist, no metadata created!")
         return None  # No metadata if no .parquet files exist
 
-    schema = pq.ParquetDataset(parquet_path, use_legacy_dataset=False).schema
+    schema = pq.ParquetDataset(parquet_path).schema
     if schema.metadata:
         metadata = {k.decode(): v.decode() for k, v in schema.metadata.items()}
     else:
